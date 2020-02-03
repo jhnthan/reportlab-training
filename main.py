e32d0e49680eb78ae69d7eef059124296dd17078
JGCH <g.ch.jhonathan@gmail.com>
2020-02-03 16:48:49 -0500

    add file main with the a simple implementation

--- main.py
+++ main.py
@@ -23,3 +23,105 @@ from reportlab.lib.units import mm
 from reportlab.lib.styles import ParagraphStyle
 
 from io import BytesIO
+
+f = []
+for x in range(0,16):
+  f.append(
+    Paragraph(
+      "This is a Paragraph " + str(x),
+      ParagraphStyle(
+          'MyStyle1',
+          fontSize=10,
+      )
+    )
+  )
+
+h = []
+for x in range(0,16):
+  h.append(
+    Paragraph(
+      "Other page" + str(x),
+      ParagraphStyle(
+          'MyStyle1',
+          fontSize=10,
+      )
+    )
+  )
+
+content = []
+content.append(
+  Paragraph(
+    "This is a Paragraph ",
+    ParagraphStyle(
+      'MyStyle1',
+      fontSize=10,
+    )
+  )
+)
+content.append(
+  Paragraph(
+    "This is a Paragraph 2",
+    ParagraphStyle(
+      'MyStyle1',
+      fontSize=10,
+    )
+  )
+)
+content.append(
+  Paragraph(
+    "This is a Paragraph 5",
+    ParagraphStyle(
+      'MyStyle1',
+      fontSize=10,
+    )
+  )
+)
+content.append(NextPageTemplate('page2'))
+content.append(
+  Paragraph(
+    "This is a Paragraph 3",
+    ParagraphStyle(
+      'MyStyle1',
+      fontSize=10,
+    )
+  )
+)
+content.append(
+  Paragraph(
+    "This is a Paragraph 4",
+    ParagraphStyle(
+      'MyStyle1',
+      fontSize=10,
+    )
+  )
+)
+
+frame1 = Frame(
+  x1=0,
+  y1=0,
+  width=50 * mm,
+  height=20 * mm,
+  leftPadding=0,
+  bottomPadding=0,
+  rightPadding=0,
+  topPadding=0,
+  id='frame1',
+  showBoundary=0.1)
+
+frame2 = Frame(
+  x1=0,
+  y1=0,
+  width=50 * mm,
+  height=8 * mm,
+  leftPadding=0,
+  bottomPadding=0,
+  rightPadding=0,
+  topPadding=0,
+  id='frame2',
+  showBoundary=0.1)
+page1 = PageTemplate(id='page1',frames=[frame1],pagesize=(100*mm,100*mm))
+page2 = PageTemplate(id='page2',frames=[frame2],pagesize=(80*mm,80*mm))
+doc = BaseDocTemplate(filename=BytesIO(), showBoundary=1)
+doc.addPageTemplates(page1)
+doc.addPageTemplates(page2)
+doc.build(content, filename="Training.pdf")
